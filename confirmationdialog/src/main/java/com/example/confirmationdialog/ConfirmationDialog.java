1ed64cd70bcee91d3aff103834ad1a3302b66f5d
aly <alyabdelshafy6561@gmail.com>
2019-11-25 14:27:42 +0200

    add methods to customize dialog

--- confirmationdialog/src/main/java/com/example/confirmationdialog/ConfirmationDialog.java
+++ confirmationdialog/src/main/java/com/example/confirmationdialog/ConfirmationDialog.java
@@ -2,9 +2,11 @@ package com.example.confirmationdialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,7 +16,8 @@ public class ConfirmationDialog extends Dialog implements View.OnClickListener{
     private Context context;
     private ConfirmationDialogInterface confirmationDialogInterface;
     private Button positiveButton,negativeButton;
-    private TextView contentTextView;
+    private TextView contentTextView,headerTextView;
+    private LinearLayout rootView;
 
 
     public ConfirmationDialog(@NonNull Context context, @StyleRes int themeResId,
@@ -38,6 +41,8 @@ public class ConfirmationDialog extends Dialog implements View.OnClickListener{
         positiveButton = findViewById(R.id.btn_positive);
         negativeButton = findViewById(R.id.btn_negative);
         contentTextView = findViewById(R.id.tv_content);
+        rootView = findViewById(R.id.root_view);
+        headerTextView = findViewById(R.id.tv_header);
 
     }
 
@@ -66,11 +71,40 @@ public class ConfirmationDialog extends Dialog implements View.OnClickListener{
         void onPositiveClicked();
     }
 
-
-
-
     @Override
     public void cancel() {
         super.cancel();
+    }
+
+
+    public void setBackGround(Drawable drawable){
+        rootView.setBackground(drawable);
+    }
+
+    public void setHeaderText(String headerText){
+        headerTextView.setText(headerText);
+    }
+
+
+    public void contentText(String content){
+        headerTextView.setText(content);
+    }
+
+    public void setPositiveButtonText(String text){
+        positiveButton.setText(text);
+    }
+
+    public void setNegativeButtonText(String text){
+        negativeButton.setText(text);
+    }
+
+
+    public void setPositiveButtonBackGround(Drawable drawable){
+        positiveButton.setBackground(drawable);
+    }
+
+
+    public void setNegativeButtonBackGround(Drawable drawable){
+        negativeButton.setBackground(drawable);
     }
 }
